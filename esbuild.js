3d0d782a46a666c965cac2ae9aa47204bcdaeb10
Manybugs <me@manybugs.dev>
2025-11-28 23:48:42 +0900

    esbuild client code

--- esbuild.js
+++ esbuild.js
@@ -1,56 +1,66 @@
 const esbuild = require("esbuild");
 
-const production = process.argv.includes('--production');
-const watch = process.argv.includes('--watch');
+const production = process.argv.includes("--production");
+const watch = process.argv.includes("--watch");
 
 /**
  * @type {import('esbuild').Plugin}
  */
 const esbuildProblemMatcherPlugin = {
-	name: 'esbuild-problem-matcher',
+  name: "esbuild-problem-matcher",
 
-	setup(build) {
-		build.onStart(() => {
-			console.log('[watch] build started');
-		});
-		build.onEnd((result) => {
-			result.errors.forEach(({ text, location }) => {
-				console.error(`✘ [ERROR] ${text}`);
-				console.error(`    ${location.file}:${location.line}:${location.column}:`);
-			});
-			console.log('[watch] build finished');
-		});
-	},
+  setup(build) {
+    build.onStart(() => {
+      console.log("[watch] build started");
+    });
+    build.onEnd((result) => {
+      result.errors.forEach(({ text, location }) => {
+        console.error(`✘ [ERROR] ${text}`);
+        console.error(
+          `    ${location.file}:${location.line}:${location.column}:`
+        );
+      });
+      console.log("[watch] build finished");
+    });
+  },
 };
 
 async function main() {
-	const ctx = await esbuild.context({
-		entryPoints: [
-			'src/extension.ts'
-		],
-		bundle: true,
-		format: 'cjs',
-		minify: production,
-		sourcemap: !production,
-		sourcesContent: false,
-		platform: 'node',
-		outfile: 'dist/extension.js',
-		external: ['vscode'],
-		logLevel: 'silent',
-		plugins: [
-			/* add to the end of plugins array */
-			esbuildProblemMatcherPlugin,
-		],
-	});
-	if (watch) {
-		await ctx.watch();
-	} else {
-		await ctx.rebuild();
-		await ctx.dispose();
-	}
+  const defaultSettings = {
+    bundle: true,
+    minify: production,
+    sourcemap: !production,
+    sourcesContent: false,
+    logLevel: "silent",
+  };
+  const client = await esbuild.context({
+    ...defaultSettings,
+    entryPoints: ["src/client/main.ts"],
+    format: "esm",
+    platform: "browser",
+    outfile: "dist/main.js",
+  });
+  const ctx = await esbuild.context({
+    ...defaultSettings,
+    entryPoints: ["src/extension.ts"],
+    format: "cjs",
+    platform: "node",
+    outfile: "dist/extension.js",
+    external: ["vscode"],
+    plugins: [
+      /* add to the end of plugins array */
+      esbuildProblemMatcherPlugin,
+    ],
+  });
+  if (watch) {
+    await Promise.all([client.watch(), ctx.watch()]);
+  } else {
+    await Promise.all([client.rebuild(), ctx.rebuild()]);
+    await Promise.all([client.dispose(), ctx.dispose()]);
+  }
 }
 
-main().catch(e => {
-	console.error(e);
-	process.exit(1);
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
 });
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,12 @@
 import * as vscode from "vscode";
 
+const getPath =
+  (context: vscode.ExtensionContext, panel: vscode.WebviewPanel) =>
+  (...path: string[]) =>
+    panel.webview.asWebviewUri(
+      vscode.Uri.joinPath(context.extensionUri, ...path)
+    );
+
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand("novelize.novelize", async () => {
@@ -11,28 +18,14 @@ export function activate(context: vscode.ExtensionContext) {
           enableScripts: true,
         }
       );
-      // Get path to resource on disk
-      const dirPath = vscode.Uri.joinPath(context.extensionUri, "client");
-      const { stylecss, inkjs, mainjs, storyjs } = [
-        "style.css",
-        "ink.js",
-        "main.js",
-        "story.js",
-      ].reduce(
-        (obj, name) => ({
-          ...obj,
-          [name.replace(".", "")]: panel.webview.asWebviewUri(
-            vscode.Uri.joinPath(dirPath, name)
-          ),
-        }),
-        {} as Record<string, vscode.Uri>
-      );
-      panel.webview.html = html([stylecss], [inkjs, storyjs, mainjs]);
+      const path = getPath(context, panel);
+      const mainjs = path("dist", "main.js");
+      panel.webview.html = html(mainjs);
     })
   );
 }
 
-function html(styles: vscode.Uri[], scripts: vscode.Uri[]) {
+function html(mainjs: vscode.Uri) {
   return `
 <!doctype html>
 <html lang="en">
@@ -43,17 +36,11 @@ function html(styles: vscode.Uri[], scripts: vscode.Uri[]) {
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    ${styles.map((src) => `<link rel="stylesheet" href="${src}">`).join("")}
-
 </head>
 
 <body>
-
-    <div id="story" class="container">
-        <h1>The Intercept</h1>
-    </div>
-
-    ${scripts.map((src) => `<script src="${src}"></script>`).join("")}
+  <div id="root"></div>
+  <script type="module" src="${mainjs}"></script>
 </body>
 </html>  
   `;
