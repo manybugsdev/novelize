9fd6128f6d390401b2fe94f64e813220d77bbd3d
Manybugs <me@manybugs.dev>
2025-11-30 22:09:24 +0900

    Revert "esbuild client code"
    This reverts commit 3d0d782a46a666c965cac2ae9aa47204bcdaeb10.

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,5 @@
 import * as vscode from "vscode";
 
-const getPath =
-  (context: vscode.ExtensionContext, panel: vscode.WebviewPanel) =>
-  (...path: string[]) =>
-    panel.webview.asWebviewUri(
-      vscode.Uri.joinPath(context.extensionUri, ...path)
-    );
-
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand("novelize.novelize", async () => {
@@ -18,14 +11,28 @@ export function activate(context: vscode.ExtensionContext) {
           enableScripts: true,
         }
       );
-      const path = getPath(context, panel);
-      const mainjs = path("dist", "main.js");
-      panel.webview.html = html(mainjs);
+      // Get path to resource on disk
+      const dirPath = vscode.Uri.joinPath(context.extensionUri, "client");
+      const { stylecss, inkjs, mainjs, storyjs } = [
+        "style.css",
+        "ink.js",
+        "main.js",
+        "story.js",
+      ].reduce(
+        (obj, name) => ({
+          ...obj,
+          [name.replace(".", "")]: panel.webview.asWebviewUri(
+            vscode.Uri.joinPath(dirPath, name)
+          ),
+        }),
+        {} as Record<string, vscode.Uri>
+      );
+      panel.webview.html = html([stylecss], [inkjs, storyjs, mainjs]);
     })
   );
 }
 
-function html(mainjs: vscode.Uri) {
+function html(styles: vscode.Uri[], scripts: vscode.Uri[]) {
   return `
 <!doctype html>
 <html lang="en">
@@ -36,11 +43,17 @@ function html(mainjs: vscode.Uri) {
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    ${styles.map((src) => `<link rel="stylesheet" href="${src}">`).join("")}
+
 </head>
 
 <body>
-  <div id="root"></div>
-  <script type="module" src="${mainjs}"></script>
+
+    <div id="story" class="container">
+        <h1>The Intercept</h1>
+    </div>
+
+    ${scripts.map((src) => `<script src="${src}"></script>`).join("")}
 </body>
 </html>  
   `;
